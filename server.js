06e3c83bc719a60ab8f1878673eeed957fa5ecaf
Williams Isaac <williamscalg@gmail.com>
2018-04-20 05:47:56 +0100

    changed server.js folder

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-//Install express server
 const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+
+const port = 8080;
+
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(helmet());
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-  
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, 'dist')));
+app.all('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'));
+
+app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`));
